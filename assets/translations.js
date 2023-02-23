fc5f61c891511743cfde6e222aaa19a9b9fd0c9c
Trif Adriana <trif.corina.adriana@gmail.com>
2023-02-23 17:33:13 +0200

    RTL layout fixes

--- assets/translations.js
+++ assets/translations.js
@@ -24,7 +24,7 @@ const translations = {
     OF: 'de'
   },
   ar: {
-    ROWS_PER_PAGE: ':دد الصفوف في الصفحة',
+    ROWS_PER_PAGE: 'دد الصفوف في الصفحة:', // The browser moves the collon to the left
     OF: 'من'
   }
 };
